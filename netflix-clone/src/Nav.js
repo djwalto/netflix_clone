a70f0a9f8dba41cf8b4d36370a95ce1bc2fa7292
David Walton <davidwalton6772@gmail.com>
2020-09-30 14:19:40 -0500

    nav logo and user avatar

---
+++ netflix-clone/src/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Nav.css';
+
+function Nav() {
+    return (
+        <div>
+            <div className="nav">
+                <img className="nav_logo"
+                    src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png" alt="Netflix Logo"></img>
+                <img className="nav_avatar"
+                    src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="Netflix Logo"></img>
+            </div>
+        </div>
+    )
+}
+
+export default Nav
